e4d584f60143f3112234f33553e8ef0ae7dfcbba
Swastik Pradhan <62826998+Rdx123942@users.noreply.github.com>
2024-06-12 22:01:58 +0530

    new user name

--- lms/login.js
+++ lms/login.js
@@ -1,6 +1,8 @@
 const users = [
     { username: 'sohamsawalakhe@gmail.com', password: '7558496659', name: 'Soham Manish Sawalakhe', email: 'sohamsawalakhe@gmail.com', courses: ['PYAIDS', 'Python'] },
     { username: 'kanishklaptop1@gmail.com', password: '8855055725', name: 'Kanishk Butle ', email: 'kanishklaptop1@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'keerthikeerthana16074@gmail.com', password: '9344657858', name: 'Keerthana s', email: 'keerthikeerthana16074@gmail.com', courses: ['PYAIDS', 'Python'] },
+
 
 ];
 
