13688ad5e634c0274eba718ac89b7be071b7a798
Swastik Pradhan <62826998+Rdx123942@users.noreply.github.com>
2024-07-05 03:30:35 +0530

    username update

--- lms/login.js
+++ lms/login.js
@@ -3,6 +3,12 @@ const users = [
     { username: 'kanishklaptop1@gmail.com', password: '8855055725', name: 'Kanishk Butle ', email: 'kanishklaptop1@gmail.com', courses: ['PYAIDS', 'Python'] },
     { username: 'keerthikeerthana16074@gmail.com', password: '9344657858', name: 'Keerthana s', email: 'keerthikeerthana16074@gmail.com', courses: ['PYAIDS', 'Python'] },
     { username: 'patturisindhuja2003@gmail.com', password: '6303513389', name: 'Sindhuja', email: 'patturisindhuja2003@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'himanshusah1211@gmail.com', password: '9910986719', name: 'Himanshu Sah', email: 'himanshusah1211@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'rakshitharakshithacm@gmail.com', password: '9019056966', name: 'RAKSHITHA C M ', email: 'rakshitharakshithacm@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'bhanothyaswanth2801@gmail.com', password: '9110723142', name: 'Bhanoth Yaswanth Sai ', email: 'bhanothyaswanth2801@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'hk.sneha27@gmail.com', password: '7303054993', name: 'sneha', email: 'hk.sneha27@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'aadhithsekar@gmail.com', password: '9360650158', name: 'Adith.S', email: 'aadhithsekar@gmail.com', courses: ['PYAIDS', 'Python'] },
+    { username: 'umesh_kumar.ug22@nsut.ac.in', password: '7340423590', name: 'UMESH KUMAR', email: 'umesh_kumar.ug22@nsut.ac.in', courses: ['PYAIDS', 'Python'] },
 
 
 ];
